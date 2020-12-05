307abf9d480eedcd998cf10aefbb21a57e0256d9
isak <Isak.Eriksson@sartorius.com>
2020-12-05 19:10:15 +0100

    day_05

---
+++ src/advent_of_code_2020/day_05.clj
@@ -0,0 +1,47 @@
+(ns advent-of-code-2020.day-05
+  (:require [clojure.string :as str]
+            [clojure.set :as s]))
+
+(def input (-> (slurp "resources/day_05/input")
+               (str/split #"\r\n")))
+
+(defn seat-id [{:keys [col row]}]
+  (+ col (* 8 row)))
+
+(def rows (range 128))
+(def cols (range 8))
+
+(defn search [bin?]
+  (fn [s x]
+    (if (bin? x)
+      (drop (/ (count s) 2) s)
+      (take (/ (count s) 2) s))))
+
+(defn find-seat [seat-text]
+  (let [[row col] (split-at 7 seat-text)]
+    {:row (first (reduce (search #(= \B %)) rows row))
+     :col (first (reduce (search #(= \R %)) cols col))}))
+
+;; part 1
+(->> input
+     (map find-seat)
+     (map seat-id)
+     (apply max))
+
+;; part 2
+(def all-ids (apply sorted-set (range 1024)))
+
+(defn non-consecutive? [s]
+  (let [[a b] s]
+    (not= (+ 1 a) b)))
+
+(->> input
+    (map find-seat)
+    (map seat-id)
+    (apply sorted-set)
+    (s/difference all-ids)
+    (partition 2 1)
+    (filter non-consecutive?)
+    (map set)
+    (apply s/intersection)
+    first)
